b01e6a3a8304739e159f6cf5a293b519fd72e489
Victor Castro <vicastro@pdx.edu>
2024-07-11 22:44:24 -0700

    Creating the first inputs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Calculator {
+
+    static void Main(string[] args) {
+        Console.Write("\nWelcome to the calculator app!");
+
+        Console.Write("\nPlease enter your first number: ");
+        float num1 = Convert.ToSingle(Console.ReadLine());  
+
+        Console.Write("\nPlease enter the second number: ");
+        float num2 = Convert.ToSingle(Console.ReadLine());  
+
+        Console.WriteLine($"\nYou have entered {num1} and {num2}");
+    }
+
+}
